ef1c393da89cb2949aacfa283114d508824c5eed
witty-banner-1950 <asimsali95@gmail.com>
2019-06-18 21:34:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,11 @@ def roll_call_dwarves (dwarves)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet (veggies)
   # Your code here
+  veggies.collect do |call|
+    call.capitalize + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
